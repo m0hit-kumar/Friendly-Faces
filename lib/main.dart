c3a5927bea6bf0b71303dc818e51c92d75f5eb13
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2023-02-12 03:30:19 +0530

    create profile Page ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:friendly_faces/pages/auth/auth_page.dart';
+import 'package:friendly_faces/pages/auth/create_profile.dart';
 import 'package:friendly_faces/pages/auth/otp_page.dart';
 import 'package:get/get.dart';
 import 'firebase_options.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialRoute: "/auth",
+      initialRoute: "/createProfile",
       debugShowCheckedModeBanner: false,
       getPages: [
         GetPage(
@@ -30,6 +31,10 @@ class MyApp extends StatelessWidget {
         GetPage(
           name: "/otpPage",
           page: () => const AdminOTPScreen(),
+        ),
+        GetPage(
+          name: "/createProfile",
+          page: () => const CreateProfile(),
         ),
       ],
     );
---
+++ lib/pages/auth/create_profile.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:friendly_faces/constants/constants.dart';
+import 'package:friendly_faces/widgets/custom_button.dart';
+import 'package:friendly_faces/widgets/input_widget.dart';
+import 'package:get/get.dart';
+
+class CreateProfile extends StatefulWidget {
+  const CreateProfile({super.key});
+
+  @override
+  State<CreateProfile> createState() => _CreateProfileState();
+}
+
+class _CreateProfileState extends State<CreateProfile> {
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final constants = Get.put(Constants());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: Get.height,
+          width: Get.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [constants.centerLeftColor, constants.centerRightColor],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50.0, left: 30.0, right: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Verification Page",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: constants.whiteBackground,
+                      fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    "Name",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xffa3b1c6), // darker color
+                      ),
+                      BoxShadow(
+                        color: Color(0xffe0e5ec), // background color
+                        spreadRadius: -12.0,
+                        blurRadius: 12.0,
+                      ),
+                    ],
+                    color: constants.inputBackgroundColor,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: constants.whiteBackground),
+                  ),
+                  // width: 60,
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: " ",
+                        border: InputBorder.none,
+                      ),
+                      autofocus: false,
+                      style: const TextStyle(
+                          fontSize: 18, color: Color.fromARGB(255, 19, 19, 19)),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    "Email",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xffa3b1c6), // darker color
+                      ),
+                      BoxShadow(
+                        color: Color(0xffe0e5ec), // background color
+                        spreadRadius: -12.0,
+                        blurRadius: 12.0,
+                      ),
+                    ],
+                    color: constants.inputBackgroundColor,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: constants.whiteBackground),
+                  ),
+                  // width: 60,
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: " ",
+                        border: InputBorder.none,
+                      ),
+                      autofocus: false,
+                      style: const TextStyle(
+                          fontSize: 18, color: Color.fromARGB(255, 19, 19, 19)),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    "DOB",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xffa3b1c6), // darker color
+                      ),
+                      BoxShadow(
+                        color: Color(0xffe0e5ec), // background color
+                        spreadRadius: -12.0,
+                        blurRadius: 12.0,
+                      ),
+                    ],
+                    color: constants.inputBackgroundColor,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: constants.whiteBackground),
+                  ),
+                  // width: 60,
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: " ",
+                        border: InputBorder.none,
+                      ),
+                      autofocus: false,
+                      style: const TextStyle(
+                          fontSize: 18, color: Color.fromARGB(255, 19, 19, 19)),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    "Proffesion",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xffa3b1c6), // darker color
+                      ),
+                      BoxShadow(
+                        color: Color(0xffe0e5ec), // background color
+                        spreadRadius: -12.0,
+                        blurRadius: 12.0,
+                      ),
+                    ],
+                    color: constants.inputBackgroundColor,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: constants.whiteBackground),
+                  ),
+                  // width: 60,
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: " ",
+                        border: InputBorder.none,
+                      ),
+                      autofocus: false,
+                      style: const TextStyle(
+                          fontSize: 18, color: Color.fromARGB(255, 19, 19, 19)),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          // color: constants.whiteBackground,
+                          border: Border.all(width: 2, color: Colors.white),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(5.0),
+                          ),
+                        ),
+                        width: Get.width / 3,
+                        height: 50,
+                        child: const Center(
+                          child: Text("Cancel",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Color.fromARGB(255, 53, 52, 52), //New
+                                blurRadius: 2.0,
+                                spreadRadius: 0.3)
+                          ],
+                          color: constants.whiteBackground,
+                          // border: Border.all(width: 2, color: Colors.white),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(5.0),
+                          ),
+                        ),
+                        width: Get.width / 3,
+                        height: 50,
+                        child: const Center(
+                          child: Text("Cancel",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
